a107bf2598d6507e700fb614a0fab1f9273d736c
AHBAIZ Mouhcine <mahbaiz@enseirb-matmeca.fr>
2023-08-03 11:38:41 +0100

    Unittest added pt 2

--- modules/TI/threat_intelligence.py
+++ modules/TI/threat_intelligence.py
@@ -2,120 +2,87 @@ import os
 import subprocess
 import json
 import requests
-
+from . import download
 
 #Fills the database according to the path of the feed folder
 #params:
 #download_file: a bash script that download links in the feed_folder
 #abuse_api_key: calls the abuseIPDB API to fill the file abuseIPDB.txt used also as a feed to the TI module
 
-def fill_db(download_file, abuse_api_key, feed_folder):
-    download_file = os.path.expanduser(download_file)
-    #fill_database_with_abuseipdb(abuse_api_key, feed_folder)
-    try :
-        subprocess.run(['bash', download_file], check = True)
-        return True
-    except subprocess.CalledProcessError as e:
-        print(f"Error occured while filling the TI database: {e}")
-        return False
+class ThreatIntelligence:
 
-#Searchs for the given ip in the feed_folder
-def search_for_ip(ip, feed_folder):
-    path = None
-    feed_folder = os.path.expanduser(feed_folder)+"/"
-    is_found = False
-    for feed_file in os.listdir(feed_folder):
-        with open(feed_folder+feed_file, 'r') as f:
-            for line_number, line in enumerate(f, start = 1):
-                if ip in line:
-                    path=f"{feed_file},  Line: {line_number}"
-                    is_found = True
+    def __init__(self, TI_feed, key, feed_folder):
+        if not TI_feed or not key or not feed_folder:
+            raise TypeError("Arguments cannot be None or empty")
+        self.download_file = os.path.expanduser(TI_feed)
+        self.abuse_api_key = key
+        self.feed_folder = feed_folder
+        
+    def fill_db(self):
+        #fill_database_with_abuseipdb(abuse_api_key, feed_folder)
+        # try :
+        #     subprocess.run(['bash', self.download_fil], check = True)
+        #     return True
+        # except subprocess.CalledProcessError as e:
+        #     print(f"Error occured while filling the TI database: {e}")
+        #     return False
 
-    return (is_found, path)
+        return download.download_files(os.path.dirname(self.feed_folder))
+        
+        #Searchs for the given ip in the feed_folder
+    def search_for_ip(self, ip, feed_folder):
+        path = None
+        feed_folder = os.path.expanduser(feed_folder)+"/"
+        is_found = False
+        for feed_file in os.listdir(feed_folder):
+            with open(feed_folder+feed_file, 'r') as f:
+                for line_number, line in enumerate(f, start = 1):
+                    if ip in line:
+                        path=f"{feed_file},  Line: {line_number}"
+                        is_found = True
+                        
+        return (is_found, path)
+                    
+    # def search_for_domain(domain):
+    #     feed_folder = "feed"
+    #     is_found = 0
+    #     for feed_file in os.listdir(feed_folder):
+    #         print(feed_file)
+    #         with open("feed/"+feed_file, 'r') as f:
+    #             for line_number, line in enumerate(f, start = 1):
+    #                 if domain in line:
+    #                     print("File: ", feed_file,  "\tLine: " , line_number)
+    #                     is_found = 1
     
-# def search_for_domain(domain):
-#     feed_folder = "feed"
-#     is_found = 0
-#     for feed_file in os.listdir(feed_folder):
-#         print(feed_file)
-#         with open("feed/"+feed_file, 'r') as f:
-#             for line_number, line in enumerate(f, start = 1):
-#                 if domain in line:
-#                     print("File: ", feed_file,  "\tLine: " , line_number)
-#                     is_found = 1
-
-#     if is_found == 1:
-#         return True
-#     return False
-
-
-#Used for reporting once the ip is found malicious
-def set_malicious_ip(ip) :
-    pass
-
-def set_malicious_domain(ip) :
-    pass
-
-
-############ AbuseIPDB ###############
-
-def fill_database_with_abuseipdb(api_key, feed_folder):
-    base_url = 'https://api.abuseipdb.com/api/v2/'
-
-    # Exemple de paramètres de requête pour obtenir les adresses IP malveillantes signalées
-    query_params = {
-        'limit': 10000#,  # Nombre maximal d'adresses IP à récupérer
-        #'confidenceMinimum': 50 ,  # Niveau de confiance minimum pour les signalements
-    }
-
-    headers = {
-        'Key': api_key,
-        'Accept': 'application/json',
-    }
-
-    try:
-        response = requests.get(base_url + 'blacklist', params=query_params, headers=headers)
-        if response.status_code == 200:
-            malicious_ips = response.json().get('data', [])
-            # # Parcourir les adresses IP malveillantes et les ajouter à votre base de données
-            for ip in malicious_ips:
-                ip_address = ip['ipAddress']
-                threat_level = ip['abuseConfidenceScore']
-                country = ip['countryCode']
-                #     description = ip['abuseSeverity']
-                #     tags = ip['abuseTags']
-
-                if not os.path.exists("abuseIPDB.txt"):
-                    open("abuseIPDB.txt", 'w').close()
-                
-                with open(feed_folder+"/abuseIPDB.txt", 'a+') as feed:
-                    feed.write(str(ip_address) + ', ' + str(threat_level) + ', ' + str(country) + '\n')
-            print('La base de données a été mise à jour avec succès.')
-        else:
-            print('Erreur lors de la requête API :', response.status_code)
-    except requests.exceptions.RequestException as e:
-        print('Une erreur s\'est produite lors de la requête API :', str(e))
-
-
-########################################################################
-
-def main(ip, download_file, feed_folder, abuse_api_key):
-    #First we fill the database with malicious iocs
-    if fill_db(download_file, abuse_api_key, feed_folder):
-        print("DB filled successufully")
-
-        search_result = search_for_ip(ip, feed_folder)
-        is_found = search_result[0]
-        path = search_result[1]
-        #If the ioc is found malicious
-        if is_found :
-            return f"Malicious IP found {ip} in {path}"
-
-        else:
-            pass
-    else :
-        return "DB could not be filled"
-
-#__name__ == "__main__"
-
-# main()
+    #     if is_found == 1:
+    #         return True
+    #     return False
+    
+    
+    #Used for reporting once the ip is found malicious
+    def set_malicious_ip(self, ip) :
+        pass
+    
+    def set_malicious_domain(self, ip) :
+        pass
+    
+    
+    
+    def main(self, ip):
+        #First we fill the database with malicious iocs
+        filled = self.fill_db()
+        print("filled = ", filled)
+        if filled == True:
+            print("DB filled successufully")
+            
+            search_result = self.search_for_ip(ip, self.feed_folder)
+            is_found = search_result[0]
+            path = search_result[1]
+            #If the ioc is found malicious
+            if is_found :
+                return f"Malicious IP found {ip} in {path}"
+            
+            else:
+                pass
+        else :
+            return "DB could not be filled"
--- modules/virusTotal/vt.py
+++ modules/virusTotal/vt.py
@@ -9,41 +9,39 @@ import json
 
 
 #Calls the virusTotal API
-def query_vt_apiv3(ip, key):
-    headers = {
-        "accept": "application/json",
-        "x-apikey": key
-    }
+class virusTotal:
 
-    # if ioc.ioc_type == "ip":
-    #     url = "https://www.virustotal.com/api/v3/ip_addresses/" + ioc.ioc
+    def __init__(self, vt_api_key):
+        self.key = vt_api_key
+        
+    def query_vt_apiv3(self, ip):
+        headers = {
+            "accept": "application/json",
+            "x-apikey": self.key
+        }
+        
+        
+        url = "https://www.virustotal.com/api/v3/ip_addresses/" + ip
+        response = requests.get(url, headers=headers)
+        return json.loads(response.text)
 
-    # elif ioc.ioc_type == "domain":
-    #     url = "https://www.virustotal.com/api/v3/domains/"+ioc.ioc
-    # elif ioc.ioc_type == "url":
-    #     url = "https://www.virustotal.com/api/v3/urls/"+ioc.ioc
+    #Used to extract useful info from the API answer
+    #Different kind of returns this function could have, check the API documentation for more info
+    def interpret_response(self, response):
+        data = response["data"]
+        attributes = data["attributes"]
+        last_analysis_stats = attributes["last_analysis_stats"]
+        harmless = last_analysis_stats["harmless"]
+        malicious = last_analysis_stats["malicious"]
+        undetected = last_analysis_stats["undetected"]
+        total = harmless + malicious + undetected
+        #print(data["data"]["attributes"]["last_analysis_stats"])
+        #print("score = ", malicious,"/", total)
+        return malicious
+    
 
-    url = "https://www.virustotal.com/api/v3/ip_addresses/" + ip
-    response = requests.get(url, headers=headers)
-    return json.loads(response.text)
-
-#Used to extract useful info from the API answer
-#Different kind of returns this function could have, check the API documentation for more info
-def interpret_response(response):
-    data = response["data"]
-    attributes = data["attributes"]
-    last_analysis_stats = attributes["last_analysis_stats"]
-    harmless = last_analysis_stats["harmless"]
-    malicious = last_analysis_stats["malicious"]
-    undetected = last_analysis_stats["undetected"]
-    total = harmless + malicious + undetected
-    #print(data["data"]["attributes"]["last_analysis_stats"])
-    #print("score = ", malicious,"/", total)
-    return malicious
-
-
-def main(ip, key):
-    report = query_vt_apiv3(ip, key)
-    return interpret_response(report)
+    def main(self, ip):
+        response = self.query_vt_apiv3(ip)
+        return self.interpret_response(response)
 
 
