ff3533edd10e5fba986c043f03ab179abd4a2c15
AHBAIZ Mouhcine <mahbaiz@enseirb-matmeca.fr>
2023-08-10 20:06:30 +0100

    new way to run the main.py: python3 main.py config.json

--- README.md
+++ README.md
@@ -6,10 +6,12 @@ The Two modules accept an IP address as an input (so fat, domains can be added t
 Threat intelligence module fills  a database of malicious iocs and look for the given IP inside of it.
 
 ## configuration
-So far, the API keys are set in the config.json file, this will be changed as soon as possible.
+A config.json should be in the same folder as main.py, and must contain the two keys : "vt_key" and "abuse_key"
 
 ## Running
 The main.py contains a call for both modules
+
+python3 main.py config.json 
 
 ## Tests
 cd test; pytest -v
--- main.py
+++ main.py
@@ -2,15 +2,34 @@ import modules.TI.threat_intelligence as TI
 import modules.virusTotal.vt as VT
 import os
 import json
-
+import sys
     
 def main():
 
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py config.json")
+        return
+
+    config_file = sys.argv[1]
+
+    try:
+        with open(config_file, "r") as f:
+            config = json.load(f)
+            vt_key = config.get("vt_key")
+            abuse_key = config.get("abuse_key")
+            
+    except FileNotFoundError:
+        print(f"Le fichier de configuration {config_file} n'a pas été trouvé.")
+        return 
+    except json.JSONDecodeError:
+        print(f"Le fichier de configuration {config_file} n'est pas au format JSON valide.")
+        return 
+        
     #The ip_to_search should be extracted from the flow automatically
     ip_to_search = "88.204.166.59" 
 
     print(">>>>>>>>> VirusTotal <<<<<<<")
-    vt_key = "ea70c492edb06211d4a2f269890021c4634c21029be7e40f66afc8f6b90960a6" 
+    #vt_key = "ea70c492edb06211d4a2f269890021c4634c21029be7e40f66afc8f6b90960a6" 
     vt = VT.virusTotal(vt_key)
     print(vt.main(ip_to_search))
     
@@ -19,7 +38,7 @@ def main():
     TI_folder = os.getcwd() + "/modules/TI/"
     download_file = TI_folder + "download.sh"
     feed_folder = TI_folder + "feed"
-    abuse_key = "85f871fd5a565f2ee0f280392b108107c01c7da97feaf9b46c81d26cab08d7d32bc5002c00b81ffa"
+    #abuse_key = "85f871fd5a565f2ee0f280392b108107c01c7da97feaf9b46c81d26cab08d7d32bc5002c00b81ffa"
     ti = TI.ThreatIntelligence(download_file, abuse_key, feed_folder)
     print(ti.main(ip_to_search))
     
