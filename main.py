656a20fe22c5806cc294679e9e90d8c419c79c92
AHBAIZ Mouhcine <mahbaiz@enseirb-matmeca.fr>
2023-08-10 20:09:27 +0100

    removing hardcoded keys from main.py

--- main.py
+++ main.py
@@ -29,7 +29,6 @@ def main():
     ip_to_search = "88.204.166.59" 
 
     print(">>>>>>>>> VirusTotal <<<<<<<")
-    #vt_key = "ea70c492edb06211d4a2f269890021c4634c21029be7e40f66afc8f6b90960a6" 
     vt = VT.virusTotal(vt_key)
     print(vt.main(ip_to_search))
     
@@ -38,7 +37,6 @@ def main():
     TI_folder = os.getcwd() + "/modules/TI/"
     download_file = TI_folder + "download.sh"
     feed_folder = TI_folder + "feed"
-    #abuse_key = "85f871fd5a565f2ee0f280392b108107c01c7da97feaf9b46c81d26cab08d7d32bc5002c00b81ffa"
     ti = TI.ThreatIntelligence(download_file, abuse_key, feed_folder)
     print(ti.main(ip_to_search))
     
